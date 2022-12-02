9db252a229be762b5483663c4f4b045168bb58ea
Igor <igazarov@mail.ru>
2022-12-02 14:17:11 +0300

    3

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,22 @@
 import './index.css';
 import ReactDOM from "react-dom/client";
 import React from "react";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import App from "./App";
 import {store} from "./redux/redux-store";
 import {Provider} from "react-redux";
 
 
-
-    const root = ReactDOM.createRoot(
-        document.getElementById('root') as HTMLElement
-    );
-    root.render(
-            <BrowserRouter>
-                <Provider store={store}>
-                <App />
-                </Provider>
-            </BrowserRouter>
-    );
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+root.render(
+    <HashRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </HashRouter>
+);
 
 
 
